dcab79b2a2258876219f4489b48682c6807e73aa
Uông Đình Dũng <dungudse183885@fpt.edu.vn>
2025-11-16 15:43:43 +0700

    [BE-05]: init grade, gradeDetail

---
+++ SWD-Grading/DAL/Repository/GradeDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using DAL.Interface;
+using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class GradeDetailRepository : GenericRepository<GradeDetail, long>, IGradeDetailRepository
+    {
+        private readonly SWDGradingDbContext _context;
+        public GradeDetailRepository(SWDGradingDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
---
+++ SWD-Grading/DAL/Repository/GradeRepository.cs
@@ -0,0 +1,20 @@
+﻿using DAL.Interface;
+using Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repository
+{
+    public class GradeRepository : GenericRepository<Grade, long>, IGradeRepository
+    {
+        private readonly SWDGradingDbContext _context;
+
+        public GradeRepository(SWDGradingDbContext context) : base(context)
+        {
+            _context = context;
+        }
+    }
+}
